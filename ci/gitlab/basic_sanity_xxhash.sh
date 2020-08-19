eaf1013e30e1f15b83869ce2c7f5eab0c1afa1bc
lakshmipathi.g <lakshmipathi.g@giis.co.in>
2020-08-19 10:23:16 +0530

    Add Integrate Github with GitLabCI
    CI job does the following:
    
     - Pull latest and build btrfs-devel linux kernel
     - Setup debian rootfs image for use by QEMU
     - Pull latest btrfs-progs and apply csum patch
     - Build btrfs-progs and install dduper
     - Perform basic sanity tests
    
    Signed-off-by: lakshmipathi.g <lakshmipathi.ganapathi@collabora.com>

---
+++ ci/gitlab/basic_sanity_xxhash.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+#
+set -x
+
+dduper --help
+rm -rf /img
+truncate -s512m /img
+mkfs.btrfs -f /img --csum xxhash
+mount /img /mnt
+df 
+dd if=/dev/urandom of=/f1 bs=1M count=50
+cp /f1 /mnt/f1
+cp /f1 /mnt/f2
+loop_dev=`/sbin/losetup --find --show /img`
+sync
+dduper --device ${loop_dev} --dir /mnt --analyze
+umount /mnt
+poweroff
+
