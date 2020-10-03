8af22c9fe9640988e4d3ffc85a17f5ea0679e645
Lakshmipathi <lakshmipathi.ganapathi@collabora.com>
2020-10-03 21:13:08 +0530

    wip: add dduper stub
    Signed-off-by: Lakshmipathi <lakshmipathi.ganapathi@collabora.com>

--- src/fop.rs
+++ src/fop.rs
@@ -1,7 +1,8 @@
+use itertools::Itertools;
 use std::fs;
 use std::io;
 use std::path::PathBuf;
-use itertools::Itertools;
+mod dduper;
 
 pub fn validate_file(filename: &PathBuf) -> Result<&PathBuf, io::Error> {
     let stat = fs::metadata(filename)?;
@@ -21,14 +22,14 @@ pub fn validate_files(src_file: &PathBuf, dst_file: &PathBuf) -> Result<bool, io
     let d_file_type = dst_stat.file_type();
 
     match (s_file_type.is_file(), d_file_type.is_file()) {
-        (true,true) => return Ok(true),
+        (true, true) => return Ok(true),
         _ => return Err(io::Error::new(io::ErrorKind::Other, "Not a regular file")),
     };
 }
 
 pub fn dedupe_files(files_list: Vec<PathBuf>, dry_run: bool) {
     if dry_run {
-            println!("dry run mode");
+        println!("dry run mode");
     }
     for file in &files_list {
         // TODO: Print error message neatly
@@ -37,22 +38,26 @@ pub fn dedupe_files(files_list: Vec<PathBuf>, dry_run: bool) {
             Err(error) => println!("{:#?} Error: {}", file, error),
         };
     }
-    let comb = files_list.iter().combinations(2).collect::<Vec<_>>();
-    for f in &comb{
+    let comb: Vec<Vec<&PathBuf>> = files_list.iter().combinations(2).collect::<Vec<_>>();
+
+    for f in &comb {
         match validate_files(f[0], f[1]) {
-            Ok(_) => println!(" {:#?} {:#?} are valid files.", f[0], f[1]),
-            Err(error) => println!("Error: {}",  error),
+            Ok(_) => {
+                println!(" {:#?} {:#?} are valid files.", f[0], f[1]);
+                dduper::do_dedupe(f[0], f[1], dry_run);
+            }
+            Err(error) => println!("Error: {}", error),
         };
     }
 }
 
 pub fn dedupe_dir(dir_path: Vec<PathBuf>, dry_run: bool, recurse: bool) -> io::Result<()> {
-    let mut entries = Vec::new();
+    let mut entries: Vec<PathBuf> = Vec::new();
     if dry_run {
-            println!("dry run mode");
+        println!("dry run mode");
     }
     if recurse {
-            println!("recurse mode");
+        println!("recurse mode");
     }
     for dir in &dir_path {
         entries = fs::read_dir(dir)?
---
+++ src/fop/dduper.rs
@@ -0,0 +1,8 @@
+use itertools::Itertools;
+use std::fs;
+use std::io;
+use std::path::PathBuf;
+
+pub fn do_dedupe(src_file: &PathBuf, dst_file: &PathBuf, dry_run: bool) -> Result<bool, io::Error> {
+    todo!();
+}
